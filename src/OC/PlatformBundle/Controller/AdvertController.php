7bf56c10ba7841d5f7d668e3ecb074aa1e846b4a
jock91 <kevindesperries@gmail.com>
2015-02-11 16:57:42 +0100

    ok test

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -1,12 +1,9 @@
 <?php
-
-// src/OC/PlatformBundle/Controller/AdvertController.php
-
 namespace OC\PlatformBundle\Controller;
 
+use OC\PlatformBundle\Entity\Advert;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
@@ -47,13 +44,18 @@ class AdvertController extends Controller
 
   public function viewAction($id)
   {
-    $advert = array(
-      'title'   => 'Recherche développpeur Symfony2',
-      'id'      => $id,
-      'author'  => 'Alexandre',
-      'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-      'date'    => new \Datetime()
-    );
+  	// On récupère le repository
+    $repository = $this->getDoctrine()
+    ->getManager()
+    ->getRepository('OCPlatformBundle:Advert');
+
+
+    // On récupère l'entité correspondante à l'id $id
+    $advert = $repository->find($id);
+
+    if (null === $advert) {
+    	throw new NotFoundHttpException("L'annonce d'id".$id."n'existe pas.");
+    }
 
     return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
       'advert' => $advert
@@ -62,30 +64,28 @@ class AdvertController extends Controller
 
   public function addAction(Request $request)
   {
-  	$antispam = $this->container->get('oc_platform.antispam');
-  	$text = '...';
-  	if($antispam->isSpam($text)) {
-  		throw new \Exception('Votre message a été détécté comme spam !');
-  	}
-    // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
-    // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+    $advert = new Advert();
+    $advert->setTitle('Recherche développeur Symfony2.');
+    $advert->setAuthor('Alexandre');
+    $advert->setContent("Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…");
+
+    $em = $this->getDoctrine()->getManager();
+
+    $em->persist($advert);
+
+    $em->flush();
+
     if ($request->isMethod('POST')) {
-      // Ici, on s'occupera de la création et de la gestion du formulaire
-
       $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
-
-      // Puis on redirige vers la page de visualisation de cettte annonce
-      return $this->redirect($this->generateUrl('oc_platform_view', array('id' => 5)));
+      return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
     }
 
-    // Si on n'est pas en POST, alors on affiche le formulaire
     return $this->render('OCPlatformBundle:Advert:add.html.twig');
   }
 
   public function editAction($id, Request $request)
   {
     // Ici, on récupérera l'annonce correspondante à $id
-
     // Même mécanisme que pour l'ajout
     if ($request->isMethod('POST')) {
       $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
@@ -110,7 +110,6 @@ class AdvertController extends Controller
   {
     // Ici, on récupérera l'annonce correspondant à $id
     // Ici, on gérera la suppression de l'annonce en question
-
     return $this->render('OCPlatformBundle:Advert:delete.html.twig');
   }
 
